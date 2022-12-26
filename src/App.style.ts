d92ef51030799ea1a888c9b06ec27dddeb081b06
Caio Severino <kaiosev9@gmail.com>
2022-12-26 01:46:51 -0300

    feat: ✨ input

--- src/App.style.ts
+++ src/App.style.ts
@@ -17,4 +17,27 @@ export const StyledWrapperApp = styled.div`
       transform: scale(1.2);
     }
   }
+
+  .result {
+    position: absolute;
+    left: 20px;
+  }
+`;
+
+export const StyledInput = styled.input`
+  width: 268px;
+  height: 28px;
+  padding: 6px 48px;
+  color: rgb(0, 0, 0);
+  font-weight: 600;
+  font-family: 'Open Sans', sans-serif;
+  border-radius: 500px;
+  outline: none;
+  font-size: 0.875rem;
+`;
+
+export const StyledWrapperInput = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
 `;

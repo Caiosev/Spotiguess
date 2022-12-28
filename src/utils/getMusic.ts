a7bdad9d7ffa7477278d9afe0eda2d07a60885fe
Caio Severino <kaiosev9@gmail.com>
2022-12-28 10:39:18 -0300

    feat: ✨get music by random words

--- src/utils/getMusic.ts
+++ src/utils/getMusic.ts
@@ -1,4 +1,13 @@
+import words from 'random-words';
+
 import { Music } from '../types/Music';
+
+const valdateTrack = (data: any) => {
+  if (data.tracks.items[0].popularity < 60) {
+    return false;
+  }
+  return true;
+};
 
 export default async function getMusic(token: string): Promise<Music | null> {
   if (token !== '') {
@@ -8,10 +17,20 @@ export default async function getMusic(token: string): Promise<Music | null> {
         Authorization: `Bearer ${token}`,
       },
     };
+    const query = words({ min: 1, max: 4 }).join('-');
+
     const music = await fetch(
-      'https://api.spotify.com/v1/search?query=starboy&limit=1&type=track&market=NL',
+      `https://api.spotify.com/v1/search?query=${query}&limit=1&type=track&market=NL`,
       authParams,
-    ).then((response) => response.json().then((data) => data.tracks));
+    ).then((response) =>
+      response.json().then((data) => {
+        if (valdateTrack(data)) {
+          return data.tracks;
+        } else {
+          return getMusic(token);
+        }
+      }),
+    );
     return music || null;
   }
   return null;

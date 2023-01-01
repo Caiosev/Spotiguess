b4d74c7a370e12869a61a289673f2812fba1b07b
Caio Severino <kaiosev9@gmail.com>
2023-01-01 17:42:59 -0300

    feat: ✨ reset on restart

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 
 import { StyledWrapperApp } from './App.style';
 import Card from './components/Card/Card';
-import Audio from './components/Controller/Controller';
+import Controller from './components/Controller/Controller';
 import Input from './components/Input/Input';
 import Score from './components/Score/Score';
 import { MusicProvider } from './contexts/MusicContex';
 import { TokenProvider } from './contexts/TokenContext';
 
 function App() {
+  window.addEventListener('beforeunload', function () {
+    localStorage.setItem('streak', '0');
+  });
+
   return (
     <TokenProvider>
       <MusicProvider>
         <StyledWrapperApp>
           <Card />
-          <Audio />
+          <Controller />
           <Input />
           <Score />
         </StyledWrapperApp>
